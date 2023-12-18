8df982afd88a09f4fe23ec31844c3cf4474eb12b
janihiet <jani.hietala@metropolia.fi>
2023-12-18 18:53:26 +0200

    file hierarchy

---
+++ gpio_init_file.c
@@ -0,0 +1,9 @@
+/*
+ * Setup all GPIOs here
+ * Button
+ * LED + PWM
+ * UART TX & RX
+ * EEPROM (I2C)
+ * Piezo
+*/
+
---
+++ header.h
@@ -0,0 +1,16 @@
+//
+// Created by hiets on 18.12.2023.
+//
+
+#ifndef JVD_PROJECT_HEADER_H
+#define JVD_PROJECT_HEADER_H
+
+// c files
+#include "gpio_init_file.c"
+#include "EEPROM.c"
+#include "LED.c"
+#include "LoRaWAN.c"
+#include "rotator.c"
+
+
+#endif //JVD_PROJECT_HEADER_H
