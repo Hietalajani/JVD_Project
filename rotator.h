9e83031cfa82b68f97f39c06ea04b5598be0568e
SuperSiili <Villen.Laurikainen@gmail.com>
2023-12-28 00:02:16 +0200

    forgot rotator.h

---
+++ rotator.h
@@ -0,0 +1,21 @@
+//
+// Created by hiets on 18.12.2023.
+//
+
+#ifndef JVD_PROJECT_ROTATOR_H
+#define JVD_PROJECT_ROTATOR_H
+
+// define other stuff
+#define TURN_DIVIDER_TIMER_MS 5000
+// define pins
+#define IN1 2
+#define IN2 3
+#define IN3 6
+#define IN4 13
+#define OPTO_PIN 28
+#define PIEZO_PIN 27
+
+//testing
+#define BUTTON_PIN 12
+
+#endif //JVD_PROJECT_HEADER_H
